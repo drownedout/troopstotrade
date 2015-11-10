d021172b1f3178ee0daa04b64fcfc7f0dc664e33
drownedout <danielcdonnelly@gmail.com>
2015-11-10 11:34:09 -0500

    Added User controller, views, form testing

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+
+	def new
+	end
+
+	def create
+		render plain: params[:user].inspect
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  resources :users
+
   root 'main#index'
 
   # Example of regular route:
