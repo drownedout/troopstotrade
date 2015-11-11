4fdbba935e57bb8389151138bc643c2e2278ff40
drownedout <danielcdonnelly@gmail.com>
2015-11-10 23:17:30 -0500

    Form updated, Devise views, other CSS/Pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'main/index'
+  get 'main/about'
+  get 'main/pricing'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
