f3d1cc3331292d6b1a07f307566148ac1269deec
drownedout <danielcdonnelly@gmail.com>
2015-11-12 17:03:20 -0500

    Added more aesthetics, profile page, fixed edit sheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,18 @@ module ApplicationHelper
   	end
     nil
   end
+
+  class TrueClass
+  	def yesno
+  		"Yes"
+  	end
+  end
+
+  class FalseClass
+  	def yesno
+  		"No"
+  	end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'main/about'
   get 'main/pricing'
   get 'main/database'
+  get 'main/profile'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
