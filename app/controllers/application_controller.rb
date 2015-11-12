fd82da01658f5bdee7b4ce03048832e0e95df681
drownedout <danielcdonnelly@gmail.com>
2015-11-12 15:18:58 -0500

    bug fixes, edit profile form is now prettier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email, :password)}
-  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :password, :password_confirmation, :email, :city, :state, :zip, :phonenumber, :employmentstatus, :highesteducation, :neicertification, :militarystatus, :desiredfieldofwork, :desiredsalary, :contact)}
+  	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email, :password, :password_confirmation)}
+  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :password, :password_confirmation, :email, :city, :state, :zip, :phone, :employmentstatus, :highesteducation, :neicertification, :militarystatus, :desiredfieldofwork, :desiredsalary, :contact)}
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110165532) do
+ActiveRecord::Schema.define(version: 20151112190724) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20151110165532) do
     t.string   "city"
     t.string   "state"
     t.integer  "zip"
-    t.integer  "phonenumber"
     t.string   "employmentstatus"
     t.string   "highesteducation"
     t.string   "neicertification"
@@ -39,6 +38,7 @@ ActiveRecord::Schema.define(version: 20151110165532) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
