ca6b46871b2dc0ea9c0809e23858adbe20b6e3d0
drownedout <danielcdonnelly@gmail.com>
2015-11-13 16:33:32 -0500

    Generated 'job' model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113163634) do
+ActiveRecord::Schema.define(version: 20151113213120) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "company"
+    t.string   "url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "orgs", force: :cascade do |t|
     t.string   "company"
