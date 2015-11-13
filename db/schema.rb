05884cf80714658221d28015891ec05d949e1064
drownedout <danielcdonnelly@gmail.com>
2015-11-13 09:14:01 -0500

    Changed 'Sign In' message, added 'Last Name' to model and views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112190724) do
+ActiveRecord::Schema.define(version: 20151113135502) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20151112190724) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "phone"
+    t.string   "lastname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
