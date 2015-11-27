91c1b2b51fc273f9ca8875931a098cf35b762298
drownedout <danielcdonnelly@gmail.com>
2015-11-27 12:37:47 -0500

    More development mailer changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'sitename.com' }
   config.action_mailer.perform_deliveries = true
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
