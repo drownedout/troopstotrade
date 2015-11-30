337ee07194015d86dfe00f5a802f508ce4e79688
drownedout <danielcdonnelly@gmail.com>
2015-11-30 11:07:28 -0500

    More sendgrid changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,9 +43,9 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
-    :user_name => 'app44081149@heroku.com',
-    :password => Rails.application.secrets.sendgrid_password,
-    :domain => "sendgrid.com",
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => "heroku.com",
     :enable_starttls_auto => true
   }
 
